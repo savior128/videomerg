3c8849bc372c8530097c853c1beac7948a579239
savior128 <81635151+savior128@users.noreply.github.com>
2022-12-07 08:38:16 +0330

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,18 +4,18 @@ import os
 
 
 class Config(object):
-    API_ID = os.environ.get("API_ID")
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_ID = os.environ.get("API_ID", "14699743")
+    API_HASH = os.environ.get("API_HASH", "0cef89ed2c8025c16d2b4d42a1b8d792")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5115356918:AAFH3T-1f2x4ZdikRQnNoOXXgonLUlwryAQ")
     SESSION_NAME = os.environ.get("SESSION_NAME", "Video-Merge-Bot")
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
-    LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001625277697")
+    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001788952875")
     DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
     TIME_GAP = int(os.environ.get("TIME_GAP", 5))
     MAX_VIDEOS = int(os.environ.get("MAX_VIDEOS", 4))
-    STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME")
-    STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS")
-    MONGODB_URI = os.environ.get("MONGODB_URI")
+    STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME", "5eb945e0fc5266091de2")
+    STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS", "gLV2QdvKaRhqlrJ")
+    MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://SaviorGod:E4DY1YWGqvazdsvL@clustersavi.usbtdks.mongodb.net/?retryWrites=true&w=majority")
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
     BOT_OWNER = int(os.environ.get("BOT_OWNER", 5059280908))
 
